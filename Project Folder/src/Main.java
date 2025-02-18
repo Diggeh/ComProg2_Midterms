3ebfb5e915ebe505fcc8d2f714c750e605db6597
Diggeh <166510285+Diggeh@users.noreply.github.com>
2025-02-19 00:44:06 +0800

    Delete Project Folder directory

--- Project Folder/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Create five instances of the Person class
-       Person.People();
-       Dog.createAndManageDogs();
-       Rectangle.createAndPrintRectangle();
-    }
-}
