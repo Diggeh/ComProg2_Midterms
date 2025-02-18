1e76c935ae1cd6dcb16e89141725dec8b4098582
Diggeh <166510285+Diggeh@users.noreply.github.com>
2025-02-19 00:32:27 +0800

    Add files via upload

---
+++ Project Folder/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create five instances of the Person class
+       Person.People();
+       Dog.createAndManageDogs();
+       Rectangle.createAndPrintRectangle();
+    }
+}
---
+++ Project Folder/src/Rectangle.java
@@ -0,0 +1,30 @@
+class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double calculateArea() {
+        return width * height;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (width + height);
+    }
+
+    public void printDetails() {
+        System.out.println("\nRectangle Details:");
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        System.out.println("Area: " + calculateArea());
+        System.out.println("Perimeter: " + calculatePerimeter());
+    }
+
+    public static void createAndPrintRectangle() {
+        Rectangle rectangle = new Rectangle(5.0, 10.0);
+        rectangle.printDetails();
+    }
+}
